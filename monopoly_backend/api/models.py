9efc60d7fa42558f9e9a1e899f3f9e74d033dd7d
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 15:58:58 +0500

    add model Room

--- monopoly_backend/api/models.py
+++ monopoly_backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    max_players = models.IntegerField(default=4)
+    starting_balance = models.IntegerField(default=0)
+    creation_datetime = models.DateTimeField()
