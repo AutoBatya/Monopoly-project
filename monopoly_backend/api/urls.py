79e70c29fa52810331636d3832ad3fc6450d5193
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 16:52:57 +0500

    add create room

--- monopoly_backend/api/urls.py
+++ monopoly_backend/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("api/rooms/", views.GetAllRooms.as_view()),
     path("api/rooms/<int:id>/", views.GetRoomById.as_view()),
+    path("api/rooms/create/", views.CreateRoom().as_view())
 ]
--- monopoly_backend/api/views.py
+++ monopoly_backend/api/views.py
@@ -9,6 +9,8 @@ from django.db import models
 from .models import Room
 from .serializers import RoomSerializer
 
+
+from datetime import datetime
 # Create your views here.
 
 
@@ -33,3 +35,48 @@ class GetRoomById(APIView):
             many=False
         )
         return Response(serializer.data)
+
+class CreateRoom(APIView):
+    def get(self, request):
+        name = request.GET.get("name")
+        max_players = request.GET.get("max_players")
+        starting_balance = request.GET.get("starting_balance")
+
+        if name is None:
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        if max_players is None:
+            max_players = 4
+        else:
+            if not max_players.isdigit():
+                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            else:
+                max_players = int(max_players)
+
+        if starting_balance is None:
+            starting_balance = 0
+        else:
+            if not starting_balance.isdigit():
+                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            else:
+                starting_balance = int(starting_balance)
+
+        queryset = Room.objects.filter(name=name)
+        if len(queryset) != 0:
+            return Response(status=status.HTTP_409_CONFLICT)
+
+        room = Room()
+        room.name = name
+        room.max_players = max_players
+        room.current_players = 0
+        room.starting_balance = starting_balance
+        room.creation_datetime = datetime.now()
+        room.save()
+
+        queryset = Room.objects.filter(name=name)
+        serializer = RoomSerializer(
+            instance=queryset[0],
+            many=False
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
