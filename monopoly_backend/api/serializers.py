a907d7d5fd865655673faa08e4fb33616caaa131
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 16:17:52 +0500

    added the id field

--- monopoly_backend/api/serializers.py
+++ monopoly_backend/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 
 class RoomSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
     max_players = serializers.IntegerField(default=4)
     starting_balance = serializers.IntegerField(default=0)
