b00f7cb88711f32e68bd73ccc87e91ae72810f3f
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 16:13:06 +0500

    add get all rooms

--- monopoly_backend/api/views.py
+++ monopoly_backend/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from django.db import models
+
+from .models import Room
+from .serializers import RoomSerializer
+
 # Create your views here.
+
+
+class GetAllRooms(APIView):
+    def get(self, request):
+        queryset = Room.objects.all()
+        serializer = RoomSerializer(
+            instance=queryset,
+            many=True
+        )
+        return Response(serializer.data)
