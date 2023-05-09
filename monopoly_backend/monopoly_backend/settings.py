09a313d3b53939881f670e40d71d5bf47197da4d
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 15:58:43 +0500

    edit django project settings.py

--- monopoly_backend/monopoly_backend/settings.py
+++ monopoly_backend/monopoly_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # ---------------------------
+    "rest_framework",
+    "api"
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,17 @@ WSGI_APPLICATION = 'monopoly_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "monopoly",
+        "USER": "root",
+        "PASSWORD": "alaxyagbar_123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
