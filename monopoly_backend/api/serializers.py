e59539e63264c437640a1e82bed2b0f0d8c372fa
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-05-09 16:03:02 +0500

    add RoomSerializer

---
+++ monopoly_backend/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class RoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=50)
+    max_players = serializers.IntegerField(default=4)
+    starting_balance = serializers.IntegerField(default=0)
+    creation_datetime = serializers.DateTimeField()
