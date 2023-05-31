734d46486f9603f602f50d61ff253765a42eb0c8
dollarsin <73116502+dollarsin@users.noreply.github.com>
2023-05-31 21:22:32 +0500

    Update models.py
    add class Action

--- monopoly_backend/api/models.py
+++ monopoly_backend/api/models.py
@@ -14,6 +14,7 @@ class Room(models.Model):
 
 class User(models.Model):
     username = models.CharField(max_length=50)
+    user_id = models.IntegerField()
     balance = models.IntegerField(default=0)
     creation_datetime = models.DateTimeField()
 
@@ -22,8 +23,16 @@ class UserRoom(models.Model):
     room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     creation_datetime = models.DateTimeField(null=True)
+    
 
 class Transaction(models.Model):
     users = models.ManyToManyField(User, null=True)
     creation_datetime = models.DateTimeField(null=True)
     money =models.IntegerField(null=True)
+    
+    
+class Action(models.Model):
+    type_action = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    room_id = models.IntegerField()
+    action_time = models.DateTimeField()
