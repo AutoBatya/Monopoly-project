0204f3f5ebcf16ca7d9d73787efd8ccba3240304
dollarsin <73116502+dollarsin@users.noreply.github.com>
2023-05-31 21:28:18 +0500

    Update views.py
    add class GetActionsByUserId

--- monopoly_backend/api/views.py
+++ monopoly_backend/api/views.py
@@ -9,6 +9,9 @@ from django.db import models
 from .models import Room, User, UserRoom, Transaction
 from .serializers import RoomSerializer, UserSerializer, UserRoomSerializer, TransactionSerializer
 
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+
 from datetime import datetime
 
 
@@ -204,4 +207,42 @@ class MoneyTransfer(APIView):
             many=False
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class GetActionsByUserId(APIView):
+    def get_actions(request):
+        room_id = request.GET.get('room_id')
+        user_token = request.GET.get('user_token')
+        last_action_id = request.GET.get('last_action_id')
+
+        # Проверка, что id комнаты и токен пользователя предоставлены в запросе
+        if not room_id or not user_token:
+            return JsonResponse({'error': 'Не указаны id комнаты или токен пользователя'})
+
+        # Проверка, что id комнаты и токен пользователя действительны и что пользователь имеет доступ к этой комнате
+        try:
+            room = Room.objects.get(id=room_id)
+        except Room.DoesNotExist:
+            return JsonResponse({'error': 'Комната с указанным id не найдена'})
+
+        # Проверка доступа пользователя к комнате
+        if not room.user_set.filter(token=user_token).exists():
+            return JsonResponse({'error': 'Пользователь не имеет доступа к данной комнате'})
+
+        # Получение последнего известного действия, если оно указано в запросе
+        if last_action_id:
+            try:
+                last_action = room.action_set.get(id=last_action_id)
+            except Action.DoesNotExist:
+                return JsonResponse({'error': 'Указанный id последнего известного действия не найден'})
+
+            # Выборка всех действий для данной комнаты, которые произошли после указанного действия
+            actions = room.action_set.filter(id__gt=last_action_id)
+        else:
+            # Получение всех действий для данной комнаты
+            actions = room.action_set.all()
+
+        # Сериализация действий
+        serializer = RoomSerializer(actions, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
 
