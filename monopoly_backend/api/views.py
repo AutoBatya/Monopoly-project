cb90c5521df15c0bfda8bd1a8c897c1434355dba
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-06-01 19:41:46 +0500

    Response => JsonResponse

--- monopoly_backend/api/views.py
+++ monopoly_backend/api/views.py
@@ -25,20 +25,20 @@ class GetAllRooms(APIView):
             instance=queryset,
             many=True
         )
-        return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
 
 class GetRoomById(APIView):
     def get(self, request, id):
         queryset = Room.objects.filter(id=id)
         if queryset.count() == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND, safe=False)
 
         serializer = RoomSerializer(
             instance=queryset[0],
             many=False
         )
-        return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
 
 class CreateRoom(APIView):
@@ -48,13 +48,13 @@ class CreateRoom(APIView):
         starting_balance = request.GET.get("starting_balance")
 
         if name is None:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
 
         if max_players is None:
             max_players = 4
         else:
             if not max_players.isdigit():
-                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+                return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
             else:
                 max_players = int(max_players)
 
@@ -62,13 +62,13 @@ class CreateRoom(APIView):
             starting_balance = 0
         else:
             if not starting_balance.isdigit():
-                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+                return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
             else:
                 starting_balance = int(starting_balance)
 
         queryset = Room.objects.filter(name=name)
         if len(queryset) != 0:
-            return Response(status=status.HTTP_409_CONFLICT)
+            return JsonResponse({}, status=status.HTTP_409_CONFLICT, safe=False)
 
         room = Room()
         room.name = name
@@ -84,7 +84,7 @@ class CreateRoom(APIView):
             many=False
         )
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
 
 class CreateUser(APIView):
@@ -93,23 +93,23 @@ class CreateUser(APIView):
         username = request.GET.get("username")
         balance = request.GET.get("balance")
         if id_room is None or username is None:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
         if not id_room.isdigit():
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
         else:
             id_room = int(id_room)
         room_queryset = Room.objects.filter(id=id_room)
         if len(room_queryset) == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND, safe=False)
         room = room_queryset[0]
         if room.max_players < room.current_players + 1:
-            return Response(status=status.HTTP_409_CONFLICT)
+            return JsonResponse({}, status=status.HTTP_409_CONFLICT, safe=False)
 
         if balance is None:
             balance = room_queryset[0].starting_balance
         else:
             if not balance.isdigit():
-                return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+                return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
             else:
                 balance = int(balance)
 
@@ -118,7 +118,7 @@ class CreateUser(APIView):
             user = user_queryset[0]
             user_room_queryset = UserRoom.objects.filter(user__in=user_queryset).filter(room=room)
             if len(user_room_queryset) > 0:
-                return Response(status=status.HTTP_409_CONFLICT)
+                return JsonResponse({}, status=status.HTTP_409_CONFLICT, safe=False)
 
         user = User()
         user.username = username
@@ -140,7 +140,7 @@ class CreateUser(APIView):
             instance=user_room,
             many=False
         )
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
 
 class GetUserByIdRoom(APIView):
@@ -148,30 +148,30 @@ class GetUserByIdRoom(APIView):
         user_queryset = User.objects.filter(id=id_user)
         room_queryset = Room.objects.filter(id=id_room)
         if len(user_queryset) == 0 or len(room_queryset) == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND, safe=False)
 
         user_room_queryset = UserRoom.objects.filter(user=user_queryset[0]).filter(room=room_queryset[0])
         if len(user_room_queryset) == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND, safe=False)
 
         serializer = UserSerializer(
             instance=user_queryset[0],
             many=False
         )
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
 
 class GetUsersByIdRoom(APIView):
     def get(self, request, id_room):
         queryset = UserRoom.objects.filter(room_id=id_room)
         if len(queryset) == 0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({}, status=status.HTTP_404_NOT_FOUND, safe=False)
         user_queryset = User.objects.filter(id__in=[userroom.user_id for userroom in queryset])
         serializer = UserSerializer(
             instance=user_queryset,
             many=True
         )
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
 
 class MoneyTransfer(APIView):
@@ -180,34 +180,35 @@ class MoneyTransfer(APIView):
         sender = request.GET.get("sender_id")
         receiver = request.GET.get("receiver_id")
         if sender is None or receiver is None or not sender.isdigit() or not receiver.isdigit() or money is None or not money.isdigit():
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
         if sender == receiver:
-            return Response(status=status.HTTP_409_CONFLICT)
+            return JsonResponse({}, status=status.HTTP_409_CONFLICT, safe=False)
         sender = User.objects.filter(id=int(sender))
         receiver = User.objects.filter(id=int(receiver))
         money = int(money)
         if len(sender) != 1 or len(receiver) != 1:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
-        sender =sender[0]
-        receiver =receiver[0]
-        if sender.balance - money <0:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
+        sender = sender[0]
+        receiver = receiver[0]
+        if sender.balance - money < 0:
+            return JsonResponse({}, status=status.HTTP_406_NOT_ACCEPTABLE, safe=False)
         sender.balance -= money
         receiver.balance += money
         sender.save()
         receiver.save()
         transaction = Transaction()
         transaction.save()
-        transaction.users.add(sender,receiver)
+        transaction.users.add(sender, receiver)
         transaction.creation_datetime = datetime.now()
-        transaction.money =money
+        transaction.money = money
         transaction.save()
         serializer = TransactionSerializer(
             instance=transaction,
             many=False
         )
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+
+
 class GetActionsByUserId(APIView):
     def get_actions(request):
         room_id = request.GET.get('room_id')
@@ -232,4 +233,3 @@ class GetActionsByUserId(APIView):
         serializer = RoomSerializer(actions, many=True)
 
         return JsonResponse(serializer.data, safe=False)
-
