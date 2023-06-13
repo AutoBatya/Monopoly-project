c45e088d207d9c226f631448c758025337a4fe7f
ValeriyaFedorova <128612285+ValeriyaFedorova@users.noreply.github.com>
2023-06-13 15:48:39 +0500

    id из url

--- frontend/qrcode.js
+++ frontend/qrcode.js
@@ -14,3 +14,15 @@ const content = (qrcode) =>{
 };
 
 document.getElementById('qrcode').append(content(qrcode));
+let val=10;
+ if (history.pushState) {
+        var baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        var newUrl = baseUrl + '?id='+val;
+        history.pushState(null, null, newUrl);
+    }
+    else {
+        console.warn('History API не поддерживается');
+    }
+const url = new URL(window.location.href);  
+let id = url.searchParams.get("id");
+document.getElementById('id').innerHTML = id;
