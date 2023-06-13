bd0b933bcd89fdf4b046c73c0ca36539e6261ef6
kzkh <128722700+kzkh@users.noreply.github.com>
2023-06-13 20:23:39 +0500

    Add files via upload

--- frontend/PlayersList/main.js
+++ frontend/PlayersList/main.js
@@ -33,3 +33,14 @@ async function getResponse()
 }
 
 getResponse()
+const url = new URL(window.location.href);
+let id = url.searchParams.get("id");
+ if (history.pushState) {
+        var baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        var newUrl = baseUrl + '?id=777';
+        history.pushState(null, null, newUrl);
+    }
+    else {
+        console.warn('History API не поддерживается');
+    }  
+document.getElementById('id').innerHTML = id;
