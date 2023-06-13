f6f5d4b6c8c02dd63c0f981f972d116b358eed73
Danil Sergeev <breeennbronzoborod@yandex.ru>
2023-06-13 20:18:41 +0500

    :)

--- monopoly_backend/api/urls.py
+++ monopoly_backend/api/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/rooms/", views.GetAllRooms.as_view()),
-    path("api/rooms/<int:id>/", views.GetRoomById.as_view()),
-    path("api/rooms/create/", views.CreateRoom().as_view()),
     path("api/users/create/", views.CreateUser().as_view()),
     path("api/users/<int:id_user>/rooms/<int:id_room>/", views.GetUserByIdRoom().as_view()),
     path("api/users/rooms/<int:id_room>", views.GetUsersByIdRoom().as_view()),
