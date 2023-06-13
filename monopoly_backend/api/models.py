c39cdec1ac7b72ea56919a1b4500d6d3bd5d53fc
Denisov Makar <vsfff24@gmail.com>
2023-06-13 21:18:40 +0500

    :)

--- monopoly_backend/api/models.py
+++ monopoly_backend/api/models.py
@@ -11,19 +11,6 @@ class Room(models.Model):
     starting_balance = models.IntegerField(default=0)
     creation_datetime = models.DateTimeField()
 
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    balance = models.IntegerField(default=0)
-    creation_datetime = models.DateTimeField()
-
-
-class UserRoom(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    creation_datetime = models.DateTimeField(null=True)
-    
-
 class Transaction(models.Model):
     users = models.ManyToManyField(User)
     creation_datetime = models.DateTimeField(null=True)
--- monopoly_backend/api/serializers.py
+++ monopoly_backend/api/serializers.py
@@ -9,18 +9,6 @@ class RoomSerializer(serializers.Serializer):
     starting_balance = serializers.IntegerField(default=0)
     creation_datetime = serializers.DateTimeField()
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    username = serializers.CharField(max_length=50)
-    balance = serializers.IntegerField(default=0)
-    creation_datetime = serializers.DateTimeField()
-
-class UserRoomSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    user = UserSerializer()
-    room = RoomSerializer()
-    creation_datetime = serializers.DateTimeField()
-
 class TransactionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     users = UserSerializer(many=True)
